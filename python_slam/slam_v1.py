c7d3b36f7a63231e5f6ddc6ed32e7d15471f4653
youralien <ryan.louie@students.olin.edu>
2015-12-10 17:35:32 -0500

    added first try at matchData. (untested)

--- python_slam/slam_v1.py
+++ python_slam/slam_v1.py
@@ -34,11 +34,53 @@ class Util(object):
 
 class ScanMatcher(object):
 
-	def __init__(self):
-		pass
+	def __init__(self, io, dc):
+		# cheats
+		self.io = io
+		self.dc = dc
 
-	def matchData(self):
-		pass
+		# shared parameters
+		self.dTr = np.zeros((3,1)) # Gradient
+		self.H = np.zeros((3,3)) # Hessian
+
+	def matchData(self, beginEstimateWorld, gridMapUtil, dataContainer, covMatrix, maxIterations):
+		""" Try to stay true to the cpp API
+
+		Parameters
+		----------
+		beginEstimateWorld: array-like, shape (3,)
+			estimated pose in the world frame
+		gridMapUtil: GridMapUtil object
+			has representation of occupancy grid and methods to compute things related to the map
+		dataContainer: a dict
+			stores the scan data and its metadata
+		covMatrix: array-like, shape (3,3)
+		maxIterations: integer
+
+		Returns
+		-------
+		newEstimateWorld: array-like, shape (3,)
+		newEstimateMap: array-like, shape (3,)
+		covMatrixOut: array-like, shape (3,3)
+		"""
+		if dataContainer['size']:
+      		# Eigen::Vector3f beginEstimateMap(gridMapUtil.getMapCoordsPose(beginEstimateWorld));
+			estimate = self.io['beginEstimateMap']
+
+      		# estimateTransformationLogLh(estimate, gridMapUtil, dataContainer);
+			estimate = self.estimateTransformationLogLh(beginEstimateMap, gridMapUtil, dataContainer)
+
+			for i in range(maxIterations):
+				estimate = self.estimateTransformationLogLh(estimate, gridMapUtil, dataContainer)
+
+			estimate[2] = Util.normalize_angle(estimate[2])
+
+			covMatrixOut = self.H
+			newEsimateMap = estimate
+			newEstimateWorld = gridMapUtil.getWorldCoordsPose(estimate)
+			return newEstimateWorld, newEstimateMap, covMatrixOut
+		else:
+			return beginEstimateWorld, self.io['beginEstimateMap'], covMatrix
 
 	def estimateTransformationLogLh(self):
 		pass
